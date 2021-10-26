8496f77c6970575d24944c14ca332df2c0ba49e0
AngeloM <angelo.martinez.mt@correo.usa.edu.co>
2021-10-26 00:22:54 -0500

    first commit

---
+++ Reto3/src/main/java/Domingo_Reto3/Reto3/InterfaceReservaciones.java
@@ -0,0 +1,13 @@
+
+package Domingo_Reto3.Reto3;
+import Domingo_Reto3.Reto3.Reservaciones;
+import Domingo_Reto3.Reto3.Reservaciones;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author PEDRO
+ */
+public interface InterfaceReservaciones extends CrudRepository<Reservaciones, Integer> {
+    
+}
